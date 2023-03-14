0ee6d8a35ede95c87e4000d5aea98c2f5e306936
AsishGYT <asishsairam.illa@gytworkz.com>
2023-03-14 12:16:09 +0530

    first commit

---
+++ notes_app/app.js
@@ -0,0 +1,3 @@
+const fs = require('fs')
+
+fs.writeFileSync('notes.txt', 'This file was created by Node.js. \n This is a message to Gotham. \n You either die a hero or live long enough to see yourself become the villian.\n Because I...AM...BATMAN')
---
+++ test.js
@@ -0,0 +1,1 @@
+console.log('Welcome to Gotham city. This is Batman')
